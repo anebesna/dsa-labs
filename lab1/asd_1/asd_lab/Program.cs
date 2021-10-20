8f234aa9d55efec80f6d71956a1079e869e9cfd1
Anna <nebesna.kpi@gmail.com>
2021-10-20 23:30:49 +0300

    лабораторна готова

---
+++ lab1/asd_1/asd_lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace cs4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, k;
+            x = double.Parse(ReadLine());
+            y = double.Parse(ReadLine());
+            z = double.Parse(ReadLine());
+            k = Pow(x, -y) + z / (y * y + 1) + Cbrt(y / (x * z + 2));
+            if ((k / PI + 1 / 2) % 1 != 0 && Cbrt(x * z) != 0 && x * z >= 0 && ((x != 0 && y != 0) || y <= -1 || (x == 0 && y < 0) || (x > 0 && y != 0)) && z != 0 && Sin((x + PI * y) / z) != 0)
+            {
+                WriteLine($"a = {Tan(k)}\nb = {Tan(k) / Sin((x + PI * y) / z)}");
+            }
+            else Write("Error");
+        }
+    }
+}
---
+++ lab1/asd_2 /asd_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace asd1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter the year: ");
+            int n = int.Parse(ReadLine());
+            int k = 0;
+            int y = n % 100;
+            int c = n / 100;
+            for (int d = 0; d <= 31; d++)
+            {
+                if (((int)(2.6 * 8 - 0.2) + d + y + y / 4 + c / 4 - 2 * c) % 7 == 0)
+                {
+                    k = d;
+                }
+            }
+            Write(k);
+        }
+    }
+}
